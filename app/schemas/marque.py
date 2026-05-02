bdc897a27e0ea572025a2ab023b8a520e6a67184
Ludo35300 <ludo35300@gmail.com>
2026-05-02 17:25:53 +0200

    modif marque schema

--- app/schemas/marque.py
+++ app/schemas/marque.py
@@ -6,6 +6,7 @@ from marshmallow.validate import Length
 class MarqueSchema(Schema):
     class Meta:
         unknown = EXCLUDE
-        id       = fields.Int(dump_only=True)
-        nom      = fields.Str(required=True, validate=Length(min=1, max=100))
-        url_logo = fields.Str(allow_none=True, validate=Length(max=500))
+
+    id       = fields.Int(dump_only=True)
+    nom      = fields.Str(required=True, validate=Length(min=1, max=100))
+    url_logo = fields.Str(allow_none=True, validate=Length(max=500))
