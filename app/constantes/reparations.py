c93048703799018b086e75758a493d579aea0c04
Ludo35300 <ludo35300@gmail.com>
2026-05-14 14:55:17 +0200

    maj bug logo affichage

--- app/constantes/reparations.py
+++ app/constantes/reparations.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
+
 STATUTS_REPARATION_VALIDES = ('en_reparation', 'termine')
 STATUTS_VALIDES = ('en_attente', 'en_reparation', 'pret', 'termine')
 
-LOGO_FOLDER = "/app/static/logos"
+LOGO_FOLDER = Path("/app/static/logos")
